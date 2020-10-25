2ca84bbbb94cc426d10b9214dcc5def37e6aec84
wangzishen <wangzishen@hnu.edu.cn>
2020-10-25 12:28:51 +0800

    完成自定义首页

---
+++ components/fk-index/fk-index.vue
@@ -0,0 +1,369 @@
+<template>
+	<view class="app">
+		<!-- #ifndef H5 -->
+		<view class="status_bar" ref="status_bar"></view>
+		<!-- #endif -->
+		
+		<swiper 
+		  style="flex: 1;"
+		  :style="{height: screenHeightPx + 'px',width: screenWidthPx + 'px'}"
+		  @change="swiperChange" 
+		  @transition="transition" 
+		  @animationfinish="animationfinish"
+		  easing-function="easeInOutCubic" 
+		  duration="300"
+		  :indicator-dots="false"
+		  :current="currentFabIndex"
+		  >
+
+			<swiper-item
+				v-for="hidePage in Array(hideCount).keys()"
+				:key="'hidePage' + hidePage"
+				style="justify-content: center;align-items: center;" 
+				:style="{height: screenHeightPx + 'px',width: screenWidthPx + 'px'}"
+			>
+				<slot :name="'hidePage' + (hidePage - hideCount)"></slot>
+			</swiper-item>
+			
+			<swiper-item 
+				v-for="(page,index) in fabList"
+				:key="index"
+				style="justify-content: center;align-items: center;" 
+				:style="{height: screenHeightPx + 'px',width: screenWidthPx + 'px'}"
+			>
+				<slot :name="page.id"></slot>
+			</swiper-item>
+	
+		</swiper>
+
+		<label>
+			<uni-transition elevation="10px" :duration="150" :mode-class="modeClass" :styles="transfromClass" :show="isFabShow">
+				<view
+					v-for="(fab,index) in fabList" 
+					class="fab_item" 
+					:id="fab.id" 
+					:key="index" 
+					@click="fabClick"
+				>	
+					<image
+						mode="aspectFill"
+						:src="fab.url" 
+						style="width: 20px;height: 20px;opacity: 0.2" 
+						:style="{
+									opacity:
+										virtualCurrentFabIndex==index+hideCount
+										? virtualCurrentFabIndexOpacity 
+										: targetFabIndex==index+hideCount
+										? targetFabIndexOpacity 
+										: 0.2
+									,
+									borderRadius: 
+										fab.id=='user'
+										? '20px' 
+										: ''
+								}"
+					>
+					</image>
+					
+				</view>
+			</uni-transition>
+		</label>
+		
+	</view>
+</template>
+
+<script>
+	// #ifdef APP-NVUE
+	const Binding = uni.requireNativePlugin('bindingx');
+	// const dom = weex.requireModule('dom');
+	// #endif
+	
+	// #ifdef H5
+	import {browser} from "@/common/util.js"
+	// #endif
+
+	export default {
+		name: "fkIndex",
+		props: {
+			fabList: {
+				type: Array,
+				default() {
+					return []
+				}
+			},
+			backgroundColor: {
+				type: String,
+				default() {
+					return '#1e1e1e'
+				}
+			},
+			opacity: {
+				type: Number,
+				default() {
+					return 1
+				}
+			},
+			borderRadius: {
+				type: Number,
+				default() {
+					return 50
+				}
+			},
+			modeClass: {
+				type: Array,
+				default() {
+					return ['zoom-in','slide-bottom','fade']
+				}
+			},
+			position: {
+				type: String,
+				default() {
+					return 'right'
+				}
+			},
+			width: {
+				type: Number,
+				default() {
+					return 0.618
+				}
+			},
+			bottom: {
+				type: Number,
+				default() {
+					return 21
+				}
+			},
+			hideCount: {
+				type: Number,
+				default() {
+					return 0
+				}
+			},
+			current: {
+				type: Number,
+				default() {
+					return 0
+				}
+			}
+		},
+		data() {
+			return {
+				transfromClass: {},
+
+				isFabClick: false,
+				isFabShow: true,
+				isContinuity: 0,
+				
+				currentFabIndex: 0,
+				virtualCurrentFabIndex: 0,
+				virtualCurrentFabIndexOpacity: 1,
+				targetFabIndex: '',
+				targetFabIndexOpacity: '',
+				
+				screenHeight: '',
+				screenHeightPx: {},
+				screenWidthPx: '',
+
+			}
+		},
+		created() {
+			// 设置显示页加上负一屏的偏移
+			this.currentFabIndex = this.current + this.hideCount
+			this.virtualCurrentFabIndex = this.current + this.hideCount
+			
+			// 获取客户端系统信息
+			var system = uni.getSystemInfoSync()
+			// console.log(system)
+			this.screenHeightPx = system.screenHeight
+			this.screenHeight = system.screenHeight * 750 / system.screenWidth
+			this.screenWidthPx = system.screenWidth
+			// #ifdef H5
+			// 在H5中如果是PC端或者是iPad等大屏端就进行大屏适配
+			this.screenHeightPx = document.body.clientHeight
+			if(!browser.versions.mobile || browser.versions.iPad){
+				this.screenWidthPx = 450
+			}
+			
+			// #endif
+			console.log('屏幕高度px,rpx', this.screenHeightPx, this.screenHeight)
+			console.log('屏幕宽度px,rpx', system.screenWidth, 750)
+			
+			this.transfromClass = {
+					'position': 'fixed',
+					/* #ifndef APP-NVUE */
+					'display': 'flex',
+					/* #endif */
+					'background-color': this.backgroundColor,
+					'opacity': this.opacity,
+					'justify-content': 'space-around',
+					'align-items': 'center',
+					'border-radius': this.borderRadius + 'px',
+					'flex-direction': 'row',
+					'width': Math.max(200, this.screenWidthPx * this.width) + 'px',
+					'right': (this.screenWidthPx - Math.max(200, this.screenWidthPx * this.width)) / 2  + 'px',
+					'bottom': this.bottom + 'px',
+				}
+		},
+		methods: {
+			slideUP: function(val) {
+				if (this.isFabShow == true) {
+					this.hideFab()
+				}
+			},
+			dragDown: function(val) {
+				if (this.isFabShow == false) {
+					this.showFab()
+				}
+			},
+			transition: function(e) {
+				// 如果是点击fab进行切换，则不用展示过度动画。
+				if (this.isFabClick == true) {
+					return
+				}
+				
+				// 对连续的滑屏事件进行dx校准，在连续滑屏的时候只有一段连续的transition事件
+
+				if (e.detail.dx > 0){
+					var dx = e.detail.dx - this.screenWidthPx * this.isContinuity
+				}
+				else {
+					var dx = e.detail.dx + this.screenWidthPx * this.isContinuity
+				}
+				
+				if (dx > 0 && this.virtualCurrentFabIndex != 4) {
+					this.targetFabIndex = this.virtualCurrentFabIndex + 1
+				} 
+				else if (dx < 0 && this.virtualCurrentFabIndex != 0) {
+					this.targetFabIndex = this.virtualCurrentFabIndex - 1
+				}
+				
+				this.targetFabIndexOpacity = Math.max(1.0 * Math.abs(dx) / this.screenWidthPx, 0.2)
+				this.virtualCurrentFabIndexOpacity = Math.max(1.2 - this.targetFabIndexOpacity, 0.2)
+				
+				
+				// 优化H5连续滑屏
+				if (this.targetFabIndexOpacity > 1){
+					// console.log("连续滑动触发",this.currentFabIndex,this.virtualCurrentFabIndex,this.targetFabIndex)
+					this.isContinuity 	+= 1 
+					// if (this.isContinuity > 0){
+					// 	console.log('连续滑屏 x',this.isContinuity + 1)
+					// }
+					let real_dx = dx
+					
+					if (real_dx > 0 && this.virtualCurrentFabIndex != 4){
+						this.targetFabIndex += 1
+						this.virtualCurrentFabIndex += 1
+					}
+					else if (real_dx < 0 && this.virtualCurrentFabIndex != 0) {
+						this.targetFabIndex -= 1
+						this.virtualCurrentFabIndex -= 1
+					}
+					
+					let tempOpacity = this.virtualCurrentFabIndexOpacity
+					this.virtualCurrentFabIndexOpacity = 1
+					this.targetFabIndexOpacity = 0.2
+					// console.log("改变fab的指向",this.currentFabIndex,this.virtualCurrentFabIndex,this.targetFabIndex)
+				}
+				
+				
+			},
+			animationfinish: function() {
+				this.isContinuity = 0
+				this.virtualCurrentFabIndexOpacity = 1
+				this.targetFabIndexOpacity = 0
+				this.virtualCurrentFabIndex = this.currentFabIndex
+				this.targetFabIndex = -1
+				this.isFabClick = false
+			},
+			fabClick: function(e) {
+				if (!this.isFabShow) {
+					return
+				}
+				this.isFabClick = true
+				
+				let click_id = ''
+				
+				// #ifdef APP-NVUE
+				click_id = e.target.id
+				// #endif
+				
+				// #ifndef APP-NVUE
+				click_id = e.currentTarget.id
+				// #endif
+				
+				this.$emit('fabClick',click_id)
+				for(var i = 0; i < this.fabList.length; i++){
+					if (click_id == this.fabList[i].id) {
+						this.currentFabIndex = this.hideCount + i
+					}
+				}
+				
+				this.virtualCurrentFabIndex = this.currentFabIndex
+			},
+			swiperChange: function(e) {
+				let swiper_item_index = e.detail.current
+				this.currentFabIndex = swiper_item_index
+				if (swiper_item_index < this.hideCount) {
+					// console.log("显示负一屏，隐藏fab")
+					if (this.isFabShow == true) {
+						this.hideFab()
+					}
+				} 
+				else {
+					if (this.isFabShow == false) {
+						// console.log("隐藏负一屏，显示fab"
+						this.showFab()
+					}
+				}
+			},
+			hideFab: function() {
+				if(this.isFabShow == true){
+					this.isFabShow = false
+				}
+			},
+			showFab: function() {
+				if(this.isFabShow == false){
+					this.isFabShow = true
+				}
+			},
+			
+		}
+	}
+</script>
+
+<style scoped>
+	.app {
+		background-color: #0D0D0D;
+		flex: 1;
+		flex-direction: column;
+	}
+	.status_bar {
+		height: 30px;
+		width: 450px;
+		background-color: #0d0d0d;
+	}
+
+	.fab {
+		width: 245px;
+		height: 50px;
+		border-radius: 100px;
+		border-width: 1px;
+		background-color: #212121;
+		position: fixed;
+		left: 65px;
+		bottom: 21px;
+		flex: 1;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-around;
+	}
+
+	.fab_item {
+		height: 50px;
+		width: 50px;
+		/* background-color: #007AFF; */
+		align-items: center;
+		justify-content: center;
+	}
+
+</style>
